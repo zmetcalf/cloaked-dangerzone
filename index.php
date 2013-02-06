39c569db256c4052b23fd0367976e170648e2d98
Zachery Metcalf <zachery.metcalf@gmail.com>
2013-02-06 16:44:33 -0500

    Added session test to start checking post/session ideas

--- index.php
+++ index.php
@@ -8,8 +8,10 @@
     </head>
     <body>
         <h1>Display Text</h1>
-        <form action="">
-            <input type="button" value="Fun Button">
+        <form action="session-test/session-test.php" method="post">
+            <input type="text" name="zipcode">
+            <input type="submit" value="Fun Button" >
+            <a href="session-test/session-test.php">Session Test</a>
         </form
     </body>
 </html>
---
+++ session-test/session-test.php
@@ -0,0 +1,14 @@
+
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> </title>
+    </head>
+    <body>
+        <h1>Hello World</h1>
+        <?php echo $_POST["zipcode"]?><h1> Zip Code </h2>
+        <?php echo "Hello Spot"; ?>
+        
+    </body>
+</html>
