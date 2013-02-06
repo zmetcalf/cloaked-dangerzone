986c7d0074828d3e64cb5cd919ecdda7332f8187
Zachery Metcalf <zachery.metcalf@gmail.com>
2013-02-05 23:23:25 -0500

    Added Index and script file

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Cloaked Dangerzone</title>
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js">
+        </script>
+        <script src="script.js"></script>
+    </head>
+    <body>
+        <h1>Display Text</h1>
+        <form action="">
+            <input type="button" value="Fun Button">
+        </form
+    </body>
+</html>
