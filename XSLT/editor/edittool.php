ad5e39bb2f08f5f9c0ebfa445ed376c782f6e79f
Zachery Metcalf <zachery.metcalf@gmail.com>
2013-02-16 22:20:03 -0700

    Finished PHP XSLT system and started converting XML editor

---
+++ XSLT/editor/edittool.php
@@ -0,0 +1,30 @@
+<?php
+
+function loadFile($xml, $xsl) {
+	$xmlDoc = new DOMDocument();
+	$xmlDoc->load($xml);
+
+	$xslDoc = new DOMDocument();
+	$xslDoc->load($xsl);
+
+	$proc = new XSLTProcessor();
+	$proc->importStyleSheet($xslDoc);
+
+	echo $proc->transformToXML($xmlDoc);
+}
+
+function updateFile($xml) {
+	$xmlDoc = new DOMDocument();
+	$xmlDoc->load($xml);
+
+	loadFile($xml, "tool_updated.xsl");
+}
+
+if($_POST["btn_sub"] == "") {
+	loadFile("tool.xml", "tool.xsl");
+}
+else {
+	updateFile("tool.xml");
+}
+
+?>
--- XSLT/load-xslt.php
+++ XSLT/load-xslt.php
@@ -1,22 +1,14 @@
 <?php
-echo "top of the page";
+//sudo apt-get install php5-xsl
+//XSL is not default on ubuntu PHP
 $xmlDoc = new DOMDocument();
 $xmlDoc->load("cdcatalog.xml");
 
 $xsltTemp = new DOMDocument();
 $xsltTemp->load("cdcatalog.xsl");
 
-$proc = new XSLTProcessor;
+$proc = new XSLTProcessor();
 $proc->importStyleSheet($xsltTemp);
 
-//someone's try at this
-$xp = new XsltProcessor();
-$xp->importStylesheet($xsltTemp);
-$xp->transformToXML($xmlDoc);
-
-//echo $proc->transformToXML($xmlDoc);
-echo "not really working";
-
-
-
+echo $proc->transformToXML($xmlDoc);
 ?>
