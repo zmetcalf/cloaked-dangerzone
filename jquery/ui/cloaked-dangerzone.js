f317da6f2d10c284c3def19a1449bd587331b111
Zachery Metcalf <zachery.metcalf@gmail.com>
2013-07-25 19:44:04 -0400

    Trying toggle with comment

--- jquery/ui/cloaked-dangerzone.js
+++ jquery/ui/cloaked-dangerzone.js
@@ -1,3 +1,14 @@
 $(document).ready(function(){
     $( "#date" ).datepicker();
+    
+    function runEffect() {
+      var selectedEffect = $( "blind" ).val();
+
+      $( "#effect" ).toggle( selectedEffect, "linear",  500);
+    };
+    
+    $( "#comment-count" ).click(function() {
+      runEffect();
+      return false;
+    });
 });
