a34d29ec1d5678cfee98aa461fdf2f7e376881ad
Zach Metcalf <zachery.metcalf@gmail.com>
2013-09-22 16:21:23 -0600

    Prototype example of Mozilla

--- lib-js/js/cloaked.js
+++ lib-js/js/cloaked.js
@@ -1,29 +1,30 @@
 requirejs.config({
     baseUrl: 'js/lib',
-    
+
     shim: {
         'backbone': {
         deps: ['jquery','underscore'],
         exports: 'Backbone'
         }
     },
-    
+
     paths: {
         cloaked: '../cloaked'
     }
 });
-    
+
 requirejs([
         'jquery',
         'backbone',
-        'cloaked/sub', 
+        'cloaked/sub',
         'cloaked/backbonetutorial/model',
         'cloaked/backbonetutorial/modelrest',
         'cloaked/backbonetutorial/validate',
         'cloaked/backbonetutorial/view',
         'cloaked/backbonetutorial/approuter',
         'cloaked/backbonetutorial/collection',
-        'cloaked/backbonetutorial/addpush'
+        'cloaked/backbonetutorial/addpush',
+        'cloaked/jsexample/prototypeEx'
     ],
 
     function (
@@ -36,14 +37,15 @@ requirejs([
             view,
             approuter,
             collection,
-            addpush
-        )   
+            addpush,
+            prototypeEx
+        )
     {
-        
+
         $(document).ready(function(){
             $("p").click(function(){
                 $(this).text(sub.spot);
             });
         });
-        
+
 })
---
+++ lib-js/js/cloaked/jsexample/prototypeEx.js
@@ -0,0 +1,45 @@
+define(['backbone'], function(Backbone) {
+  //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/prototype
+
+  var Person = function() {
+    this.canTalk = true;
+    this.greet = function() {
+      if(this.canTalk) {
+        console.log("Hi, I'm " + this.name);
+      }
+    };
+  };
+
+  var Employee = function(name, title) {
+    this.name = name;
+    this.title = title;
+    this.greet = function() {
+      if(this.canTalk) {
+        console.log("Hi, I'm " + this.name + ", the " + this.title);
+      }
+    };
+  };
+  Employee.prototype = new Person();
+
+  var Customer = function(name) {
+    this.name = name;
+  };
+  Customer.prototype = new Person();
+
+  var Mime = function(name) {
+    this.name = name;
+    this.canTalk = false;
+  };
+  Mime.prototype = new Person();
+
+  var bob = new Employee('Bob', 'Builder');
+  var joe = new Customer('Joe');
+  var rg = new Employee('Red Green', 'Handyman');
+  var mike = new Customer('Mike');
+  var mime = new Mime('Mime');
+  bob.greet();
+  joe.greet();
+  rg.greet();
+  mike.greet();
+  mime.greet();
+});
